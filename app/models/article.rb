edee533a080c82dfe5e0ad44c9aeb13538def78d
Rails Udemty Course <railsudemycourse@gmail.com>
2019-07-17 18:58:40 -0400

    create new template, action, and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true, length {mininum: 10, maximum: 300}
+  validates :description, presence: true, length: {mininum: 10, maximum: 300}
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   #get 'pages/home', to: 'pages#home'
   # change to make home the main page of the server
   root 'pages#home'
-
   get 'about', to: 'pages#about'
-  #get 'about', to: 'pages#about'
+
+  # gives new article path
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
